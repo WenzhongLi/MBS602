2763c891308f9442a9a3d4a56f1678e2b4c327ac
WenzhongLi <wenzhongli7@gmail.com>
2018-03-24 11:40:38 -0400

    split key

--- average/reducer.py
+++ average/reducer.py
@@ -7,21 +7,28 @@
 import sys
 
 value_sum =0.0
-# temperature_sum = 0.0
-# pressure_sum = 0.0
-count = 0.0
+temperature_sum = 0.0
+pressure_sum = 0.0
+temperature_count = 0.0
+pressure_count = 0.0
+
 # input comes from STDIN (standard input)
 for line in sys.stdin:
-    count += 1
     # split file into line
     line = line.strip()
     # split the line into data
     key,value = line.split('\t')
     # add up
-    value_sum += float(value)
+    if key == 'temperature_average':
+        temperature_sum += float(value)
+        temperature_count += 1
+    elif key == 'pressure_sum':
+        pressure_sum += float(value)
+        pressure_count += 1
 
 
-print("average:%s" % (value_sum/count,))
+print("temperature_average:%s" % (temperature_sum/temperature_count,))
+print("pressure_average:%s" % (pressure_sum/pressure_count,))
 # print('temperature_average\t%s' % (temperature_sum/count,))
 # print('pressure_average\t%s' % (pressure_sum/count,))
 
