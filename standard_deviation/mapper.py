64c1433b7185aeb50850189a8bf205912dc54aae
WenzhongLi <wenzhongli7@gmail.com>
2018-03-24 13:13:02 -0400

    add standard_deviation

---
+++ standard_deviation/mapper.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@author: Hung
+'''
+
+import sys
+import math
+
+temperature_sum = 0.0
+pressure_sum = 0.0
+count = 0.0
+temperature_avg = 3.02812018246
+pressure_avg = 2257.852058
+# input comes from STDIN (standard input)
+# f = open("yhtest.csv",'r')
+# for line in f:
+for line in sys.stdin:
+    count += 1
+    # split file into line
+    line = line.strip()
+    # split the line into data
+    data = line.split(',')
+    # add up
+    temperature_sum += math.pow(float(data[2])-temperature_avg, 2)
+    pressure_sum += math.pow(float(data[1])-pressure_avg, 2)
+
+print('temperature_sum\t%s\t%s' % (temperature_sum, count))
+print('pressure_sum\t%s\t%s' % (pressure_sum, count))
+
---
+++ standard_deviation/reducer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@author: Hung
+'''
+
+import sys
+import math
+
+value_sum =0.0
+temperature_sum = 0.0
+pressure_sum = 0.0
+temperature_count = 0.0
+pressure_count = 0.0
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # split file into line
+    line = line.strip()
+    # split the line into data
+    key, value1, value2 = line.split('\t')
+    # add up
+    if key == 'temperature_sum':
+        temperature_sum += float(value1)
+        temperature_count += float(value2)
+    elif key == 'pressure_sum':
+        pressure_sum += float(value1)
+        pressure_count += float(value2)
+
+
+print("temperature_standard_deviation:%s" % (math.sqrt(temperature_sum/temperature_count),))
+print("pressure_standard_deviation:%s" % (math.sqrt(pressure_sum/pressure_count),))
+# print('temperature_average\t%s' % (temperature_sum/count,))
+# print('pressure_average\t%s' % (pressure_sum/count,))
+
